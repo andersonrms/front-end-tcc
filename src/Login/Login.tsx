bc28987a8c1bf38ff79d455cfe421ff6bcc6237f
Anderson Ramos <anderson.ramos@mosaico.com.br>
2023-06-14 22:38:02 -0300

    feat: create Button component

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import Image from 'next/image'
+
+import { Button, BUTTON_MODIFIERS } from '../components/Button'
 import { Input } from '../components/Input'
 import { Text } from '../components/Text'
 
@@ -8,14 +11,30 @@ const Login = () => {
     return (
         <div className={styles.Container}>
             <div>
-                <Text tag='h2'>Grafica Rapida</Text>
+                <Image 
+                    alt="Logo"
+                    src="/assets/logo.svg"
+                    width={150}
+                    height={150}
+                    loading="lazy"
+                />
+                <Text>Sistema para controle de pedidos</Text>
             </div>
 
             <div className={styles.box}>
                 <form action="">
                     <Input placeholder='Digite o seu email' />
                     <Input placeholder='Senha' />
+
+                    <Button className={styles.SignInButton} modifier={BUTTON_MODIFIERS.PRIMARY} type='submit'>Entrar</Button>
                 </form>
+                <Button className={styles.PasswordForget} modifier={BUTTON_MODIFIERS.TERTIARY} type='submit'>Esqueceu sua senha?</Button>
+
+                <div role="separator" className={styles.Divider} />
+
+                <footer>
+                    <Button className={styles.RegisterUser} modifier={BUTTON_MODIFIERS.SUPPORT} type='submit'>Criar nova conta</Button>
+                </footer>
             </div>
         </div>
     )
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import classNames from 'classnames';
+
+import { BUTTON_DEFAULT_ELEMENT, BUTTON_MODIFIERS, ButtonProps } from './types'
+import styles from './Button.module.css';
+
+const Button = <E extends React.ElementType = typeof BUTTON_DEFAULT_ELEMENT>({ 
+    as,
+    modifier = BUTTON_MODIFIERS.PRIMARY,
+    loading = false,
+    disabled = false,
+    className,
+    title,
+    children,
+    ...props
+ }: ButtonProps<E>) => {
+  const Component = as || (props.href ? 'a' : 'button');
+
+  const classname = classNames(styles.Button, styles[modifier], { 
+    [styles.Loading]: loading,
+    [styles.Disabled]: disabled,
+  }, className)
+
+  const childrenTitleFallback =
+    typeof children === 'string' ? children : undefined;
+
+  return (
+    <Component
+      className={classname}
+      disabled={disabled}
+      title={title || childrenTitleFallback}
+      target={props?.target}
+      {...props}
+    >
+        <span className={styles.Label}>{children}</span>
+    </Component>
+
+  )
+
+}
+
+export default Button
